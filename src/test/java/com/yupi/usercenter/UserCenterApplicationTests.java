f1df6c170b11e4746e723d10c374b6dd12de8a2b
zhangjialin <zhangjialin@itcast.cn>
2024-01-19 17:42:35 +0800

    第一次提交

---
+++ src/test/java/com/yupi/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,25 @@
+package com.yupi.usercenter;
+
+import com.yupi.usercenter.mapper.UserMapper;
+import com.yupi.usercenter.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+
+    @Resource
+    private UserMapper userMapper;
+    @Test
+    void contextLoads() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.isTrue(5 == userList.size(), "");
+        userList.forEach(System.out::println);
+    }
+
+}
